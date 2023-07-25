b45ca1cec1fc89e01a1c7db184fd6f84b7dc3b78
Araujo <azarujo100@gmail.com>
2023-07-25 12:01:36 -0300

    Script for Quests

---
+++ Assets/Script/Quest.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest : MonoBehaviour
+{
+    public GameObject questSC, questBTN, nextQuest;   
+    public GameObject planetIMG;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextQuest()
+    {
+        questSC.SetActive(true);
+    }
+
+    public void AnswerQuest()
+    {
+        questBTN.SetActive(false);
+        questSC.SetActive(false );        
+        planetIMG.GetComponent<Image>().enabled = true;
+        nextQuest.SetActive(true);
+        
+    }
+}
